73458ecc82b440f91709840b67ff7faea61fd498
nahotta <hottanaho@nahotta.local>
2017-10-16 21:41:32 +0900

    add front

--- web/app/com/samoyed/infrastructure/play/controller/SampleController.scala
+++ web/app/com/samoyed/infrastructure/play/controller/SampleController.scala
@@ -1,11 +1,17 @@
 package com.samoyed.infrastructure.play.controller
 
 import javax.inject.Inject
+
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
-class SampleController @Inject() extends Controller {
+class SampleController @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def index() = Action { implicit request =>
     Ok(views.html.index("Hello World"))
+  }
+
+  def create() = Action { implicit request =>
+    Ok(views.html.create())
   }
 }
